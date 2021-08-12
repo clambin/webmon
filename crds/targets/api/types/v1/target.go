b1033cfee776c9f1a98e54c86b0c8302a6cb4756
clambin <christophe.lambin@gmail.com>
2021-08-12 19:42:33 +0200

    build: generate deepcopy code for target CRD

--- crds/targets/api/types/v1/target.go
+++ crds/targets/api/types/v1/target.go
@@ -13,8 +13,9 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 )
+
+//go:generate controller-gen object paths=$GOFILE
 
 // TargetSpec contains the fields within the "spec" entry of the custom resource
 type TargetSpec struct {
@@ -23,6 +24,7 @@ type TargetSpec struct {
 }
 
 // Target layout for the custom resource
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Target struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -31,14 +33,10 @@ type Target struct {
 }
 
 // TargetList layout for a list of Target custom resources
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type TargetList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 
 	Items []Target `json:"items"`
-}
-
-// GetObjectKind returns the object's kind.  Needed to cast runtime.Object to Target in watch event handler.
-func (in *Target) GetObjectKind() schema.ObjectKind {
-	return in.TypeMeta.GetObjectKind()
 }
