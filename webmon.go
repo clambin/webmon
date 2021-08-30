ab107b9015b355f7f2cb1d1b32b5f3d46c24e5dc
clambin <christophe.lambin@gmail.com>
2021-08-30 12:23:08 +0200

    feat: graceful shutdown of prometheus server

--- webmon.go
+++ webmon.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/clambin/gotools/metrics"
 	clientV1 "github.com/clambin/webmon/crds/targets/clientset/v1"
 	"github.com/clambin/webmon/monitor"
 	"github.com/clambin/webmon/utils"
 	"github.com/clambin/webmon/version"
 	"github.com/clambin/webmon/watcher"
-	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"k8s.io/client-go/rest"
@@ -102,15 +100,12 @@ func main() {
 		}()
 	}
 
+	promServer := metrics.NewServer(8080)
+
 	go func() {
-		listenAddress := ":8080"
-		r := mux.NewRouter()
-		r.Use(prometheusMiddleware)
-		r.Path("/metrics").Handler(promhttp.Handler())
-		r.Path("/health").Handler(http.HandlerFunc(myMonitor.Health))
-		err = http.ListenAndServe(listenAddress, r)
-		if err != nil {
-			log.WithError(err).Fatal("unable to start metrics server")
+		err2 := promServer.Run()
+		if err2 != http.ErrServerClosed {
+			log.WithError(err2).Fatal("unable to start metrics server")
 		}
 	}()
 
@@ -118,6 +113,7 @@ func main() {
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	<-sigs
 
+	_ = promServer.Shutdown(30 * time.Second)
 	cancel()
 	wg.Wait()
 	log.Info("webmon stopped")
@@ -143,22 +139,4 @@ func newWatcher(monitor *monitor.Monitor, namespace string) (w *watcher.Watcher,
 	}
 
 	return watcher.NewWithClient(monitor.Register, monitor.Unregister, namespace, client), nil
-}
-
-// Prometheus metrics
-var (
-	httpDuration = promauto.NewSummaryVec(prometheus.SummaryOpts{
-		Name: "http_request_duration_seconds",
-		Help: "Duration of HTTP requests",
-	}, []string{"path"})
-)
-
-func prometheusMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		route := mux.CurrentRoute(r)
-		path, _ := route.GetPathTemplate()
-		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
-		next.ServeHTTP(w, r)
-		timer.ObserveDuration()
-	})
 }
